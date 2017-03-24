9e1fe75727c6b90d385adac009083a267de3f0b5
bastien <bastien.alvez@gmail.com>
2017-03-24 16:41:20 +0100

    optimize

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { CantineDetailsPage } from '../cantine-details/cantine-details';
 import { CantineListPage } from '../cantine-list/cantine-list';
 import {cantineService} from '../api.service';
